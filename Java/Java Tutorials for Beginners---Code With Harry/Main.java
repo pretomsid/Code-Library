990760c0a3734760c659e6a8c984621c198ef8c5
pretomsid <siddharthosen09@gmail.com>
2025-03-12 02:44:45 +0600

    Update

---
+++ Java/Java Tutorials for Beginners---Code With Harry/Main.java
@@ -0,0 +1,13 @@
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Java");
+        list.add("Python");
+        list.add("C++");
+        System.out.println(list); // Output: [Java, Python, C++] 
+    }
+}
---
+++ Java/Java Tutorials for Beginners---Code With Harry/hello_world.java
@@ -0,0 +1,6 @@
+// Description: A simple program that prints "Hello, World!" to the console.
+public class hello_world {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
